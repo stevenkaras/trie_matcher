96038a397d7ebe276cd6266fc7c080bac8a460fa
Steven Karas <steven.karas@gmail.com>
2015-08-25 18:06:42 +0200

    rewrite spec to use eq instead of be

--- spec/trie_matcher_spec.rb
+++ spec/trie_matcher_spec.rb
@@ -6,7 +6,7 @@ describe TrieMatcher do
   end
 
   it 'has a version number' do
-    expect(TrieMatcher::VERSION).not_to be nil
+    expect(TrieMatcher::VERSION).not_to eq nil
   end
 
   describe "Hash access" do
@@ -17,55 +17,55 @@ describe TrieMatcher do
     it 'should retrieve stored values' do
       value = "bar"
       @t["foo"] = value
-      expect(@t["foo"]).to be value
+      expect(@t["foo"]).to eq value
     end
 
     it 'should return the stored value' do
       value = "bar"
-      expect(@t["foo"] = value).to be value
+      expect(@t["foo"] = value).to eq value
     end
 
     it 'should store values with shared prefixes' do
       @t["cat"] = 1
       @t["car"] = 2
-      expect(@t["cat"]).to be 1
-      expect(@t["car"]).to be 2
+      expect(@t["cat"]).to eq 1
+      expect(@t["car"]).to eq 2
     end
 
     it 'should store keys that are a prefix of other keys' do
       @t["catch"] = 1
       @t["cat"] = 2
-      expect(@t["catch"]).to be 1
-      expect(@t["cat"]).to be 2
+      expect(@t["catch"]).to eq 1
+      expect(@t["cat"]).to eq 2
     end
 
     it 'should store keys that have a prefix of another key' do
       @t["cat"] = 1
       @t["catch"] = 2
-      expect(@t["cat"]).to be 1
-      expect(@t["catch"]).to be 2
+      expect(@t["cat"]).to eq 1
+      expect(@t["catch"]).to eq 2
     end
 
     it 'should do prefix searching' do
       @t["cat"] = 1
-      expect(@t["cats"]).to be 1
+      expect(@t["cats"]).to eq 1
     end
 
     it 'should do partial prefix matching' do
       @t["cat"] = 1
       @t["cats in the cradle"] = 2
-      expect(@t["cats"]).to be 1
+      expect(@t["cats"]).to eq 1
     end
 
     it 'should return the more specific prefix value' do
       @t["cat"] = 1
       @t["catch"] = 2
-      expect(@t["catcher"]).to be 2
+      expect(@t["catcher"]).to eq 2
     end
 
     it 'should not give a longer prefix value' do
       @t["catch"] = 2
-      expect(@t["cat"]).to be nil
+      expect(@t["cat"]).to eq nil
     end
   end
 
